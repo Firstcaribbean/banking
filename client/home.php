f5f3dcdfd4d1e46d74ad9d25ccb2f277786d7cbf
Firstcaribbean <146242649+Firstcaribbean@users.noreply.github.com>
2023-09-27 19:37:34 +0100

    clients

---
+++ client/home.php
@@ -0,0 +1,16 @@
+<h1 class="text-dark">Welcome </h1>
+<h1 class="text-dark"><b>Darlene Brown</b></h1>
+<?php
+?>
+<hr>
+<div class="container">
+  <div class="card">
+    <div class="card-body">
+        <h2><b>Current Balance:</b> $<?php echo number_format($_settings->userdata('balance'),2) ?></h2>
+        <h4><b>Available Balance:</b> $33,465.76</h4>
+        <h4><b>Checkings:</b> $6,500.00</h4>
+        <h4><b>Savings:</b> $6,965.76</h4>       
+    </div>
+  </div>
+</div>
+  
